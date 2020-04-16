6d3a4773f6df8819385a5a131df46568fc675328
FortyNina <nsd286@nyu.edu>
2020-04-16 12:23:57 -0500

    try to get webcam also working on desktop now

--- sketch.js
+++ sketch.js
@@ -1,27 +1,30 @@
 
 var videoFeed;
-var tracker;
 
-var rhi, ghi, bhi;
-var rlo, glo, blo;
 
 
 
 function setup() {
     var w = 640,
         h = 480;
-    videoFeed = createCapture({
-        audio: false,
-        video: {
-            width: w,
-            height: h
-        }
-    }, function() {
-        console.log('capture ready.')
-    });
-    videoFeed.elt.setAttribute('playsinline', '');
-    videoFeed.size(w, h);
-    videoFeed.parent('container');
+
+
+        videoFeed = createCapture(VIDEO);
+        videoFeed.size(w, h);
+
+
+    // videoFeed = createCapture({
+    //     audio: false,
+    //     video: {
+    //         width: w,
+    //         height: h
+    //     }
+    // }, function() {
+    //     console.log('capture ready.')
+    // });
+    // videoFeed.elt.setAttribute('playsinline', '');
+    // videoFeed.size(w, h);
+    // videoFeed.parent('container');
     cnv = createCanvas(w, h);
     cnv.parent('container');
 
