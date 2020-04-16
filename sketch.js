9e3a83d85022512e5d0ef9c722890c9e46787052
FortyNina <nsd286@nyu.edu>
2020-04-16 10:46:58 -0500

    cam only

--- sketch.js
+++ sketch.js
@@ -20,7 +20,7 @@ function setup() {
   //noCanvas();
   //createCanvas(200,200);
   createCanvas(displayWidth, displayHeight - 100);
-  
+
   if (isMobileDevice()) {
   	console.log("mobile device");
   var constraints = {
@@ -31,21 +31,15 @@ function setup() {
       }
     }
   };
-  
+
   video = createCapture(constraints);
   } else {
   	console.log("NOT mobile device");
     video = createCapture(VIDEO);
   }
-  
+
   //video.size(640, 480);
   video.hide();
-  
-  // Initialize the Image Classifier method with MobileNet and the video as the second argument
-  classifier = ml5.imageClassifier('MobileNet', video, modelReady);  
-	button = createButton('create poem');
-  //button.position(video + 20, 200 + 20);
-  button.mousePressed(greet);
 }
 
 function draw() {
@@ -53,35 +47,5 @@ function draw() {
  //image(video,0,0, 200, 200);
  fill(bgCol);
  rect(75,75,50,50);
-  
+
 }
-
-function isMobileDevice() {
-    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
-};
-
-
-function modelReady() {
-  // Change the status of the model once its ready
-  select('#status').html('Model Loaded');
-  // Call the classifyVideo function to start classifying the video
-  classifyVideo();
-}
-
-// Get a prediction for the current video frame
-function classifyVideo() {
-  classifier.predict(gotResult);
-}
-
-// When we get a result
-function gotResult(err, results) {
-  // The results are in an array ordered by probability.
-  select('#result').html(results[0].className);
-  select('#probability').html(nf(results[0].probability, 0, 2));
-  classifyVideo();
-}
-
-function greet() {
-  bgCol = random(255);
-}
-
